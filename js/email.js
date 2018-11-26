04a3e4b9d50b42002ec9e72c2abfcaf2631edd56
hkawaguchi1009 <44032593+hkawaguchi1009@users.noreply.github.com>
2018-11-26 09:49:55 -0600

    Update email.js
    finished!

--- js/email.js
+++ js/email.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    $("#myModal .btn-default").click(function() {
+       $("#noAlert").show();
+    });
+});
+
+$(document).ready(function() {
     $("#myModal .btn-primary").click(function() {
        $("#yesAlert").show();
     });
